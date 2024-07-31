cb6cef679aa8e079e5fe9ede707730920896ef30
matheus-webp <matheus.webp@gmail.com>
2024-07-31 03:28:00 -0300

    feat: add custom-gradient to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,7 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          "custom-gradient": "linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(6,23,101,1) 34%, rgba(14,51,156,1) 49%, rgba(6,59,161,1) 81%)",
       },
     },
   },
